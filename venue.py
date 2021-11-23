4f38e34d0e253800bd55e330072d9d876bb15d47
PatelAyush <patelad@myumanitoba.ca>
2021-11-22 22:37:00 -0600

    Add main.py file

--- venue.py
+++ venue.py
@@ -1,16 +1,16 @@
 import csv
 
-def venueWinPercentage(teamName):
+def venueWinPercentage():
      f=open("data/t20s_male_csv2/README.txt","r")
      listofMatchIds=[]
      L=f.readlines()
      
      for line in L:
-        if teamName in line:    
-            Id=line[42:49]
-            Id.strip()
-            listofMatchIds.append(Id)
-            #print(listofMatchIds)
+            
+        Id=line[42:49]
+        Id.strip()
+        listofMatchIds.append(Id)
+        print(listofMatchIds)
      f.close()     
 
      matchesWonhome = 0
@@ -21,9 +21,4 @@ def venueWinPercentage(teamName):
 #also have to open .info file for every match id to check if they won the match or not
 # need counter to count the number of wins
 
-     for matchId in listofMatchIds:
-          fileName=matchId.strip()
-          fileName=fileName+"countries.csv"
-          f=open("data/t20s_male_csv2/"+fileName,"r")
-          csv_f=csv.reader(f)
-          csv_f=list(csv_f)
+venueWinPercentage()
