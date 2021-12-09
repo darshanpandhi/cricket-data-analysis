a6b72c58febd1a36791fb84b0caf6b7dfd720ec6
Darshan Pandhi <darshan.pandhi@conquestplanning.com>
2021-12-08 22:12:32 -0600

    Refactor datasets.py

--- datasets.py
+++ datasets.py
@@ -6,13 +6,11 @@ import re
 
 def read_file():
     path = os.getcwd()
-    csv_files = glob.glob(os.path.join(path, "..\\cricket-data-analysis-datasets\\data\\t20s_male_csv2"
+    csv_files = glob.glob(os.path.join(path, "..\\cricket-data-analysis\\data\\t20s_male_csv2"
                                              "\\*.csv"))
     match_detail = csv_files
     regex = re.compile(r'(.*.)_info.csv$')
     match_detail = [i for i in match_detail if not regex.match(i)]
-    print(len(match_detail))
-    print(match_detail)
     df = pd.DataFrame()
     for i in match_detail:
         dataframe = pd.read_csv(i)
@@ -26,7 +24,7 @@ def read_file():
     df.to_csv('extracted_data.csv')
 
 
-def find_match_id(t1, t2):
+def find_match_ids(t1, t2):
     ls = []
     final_data = pd.read_csv('extracted_data.csv')
     for i in range(len(final_data['match_id'])):
@@ -36,14 +34,3 @@ def find_match_id(t1, t2):
         if final_data['team2'][i] == t1:
             if final_data['team1'][i] == t2:
                 ls.append(final_data['match_id'][i])
-    print(ls)
-    print("the total list of match id's for the two teams is ", len(ls))
-
-
-def main():
-    read_file()
-    find_match_id('Sri Lanka', 'Australia')
-
-
-if __name__ == "__main__":
-    main()
