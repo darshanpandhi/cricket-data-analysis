ae98d01fcc1363388cce71f476bc68af47e9141d
Darshan Pandhi <darshan.pandhi@conquestplanning.com>
2021-12-12 23:10:21 -0600

    Make prediction features optional

--- main.py
+++ main.py
@@ -15,58 +15,64 @@ def predictMatchResultByUserInput():
         getTeamRankingWinner(team1, team2, True)
     else:
         resultHtoH = getHeadToHeadWinner(team1, team2, True)
-        winner=resultHtoH
+        winner = resultHtoH
 
     print("-------------------------------------------------")
 
     print("\nPrediction with toss result known: ")
+
     teamWonToss = input("Enter the team name that won the toss: ")
-    resultToss = tossEvalTeams(team1, team2, listofMatchIds)
-    if teamWonToss == team1 and resultToss[0] > resultToss[1]:
-        winner=team1
-    elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
-        winner=team2
+
+    if teamWonToss:
+        resultToss = tossEvalTeams(team1, team2, listofMatchIds)
+        if teamWonToss == team1 and resultToss[0] > resultToss[1]:
+            winner = team1
+        elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
+            winner = team2
+
     print("Winner: ", winner)
 
-    
     print("-------------------------------------------------")
     print("\nPrediction based on venue:")
     venue = input("Enter the venue as a team name: ")
-    resultVenue=getVenuePrediction(team1,team2,venue)
-    if resultVenue:
-        winner=resultVenue
+    if venue:
+        resultVenue = getVenuePrediction(team1, team2, venue)
+        if resultVenue:
+            winner = resultVenue
+
     print("Winner: ", winner)
 
-def predictMatchResult(team1,team2,teamWonToss,venue):
-    
+
+def predictMatchResult(team1, team2, teamWonToss, venue):
     listofMatchIds = generateList(team1, team2)
 
     if len(listofMatchIds) == 0:
         getTeamRankingWinner(team1, team2, True)
-        winner=getTeamRankingWinner(team1, team2, True)
+        winner = getTeamRankingWinner(team1, team2, True)
     else:
         resultHtoH = getHeadToHeadWinner(team1, team2, True)
-        winner=resultHtoH
-    
+        winner = resultHtoH
+
         print("-------------------------------------------------")
 
         print("\nPrediction with toss result known: ")
-        resultToss = tossEvalTeams(team1, team2, listofMatchIds)
-        if teamWonToss == team1 and resultToss[0] > resultToss[1]:
-            winner=team1
-        elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
-            winner=team2
+        if teamWonToss:
+            resultToss = tossEvalTeams(team1, team2, listofMatchIds)
+            if teamWonToss == team1 and resultToss[0] > resultToss[1]:
+                winner = team1
+            elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
+                winner = team2
         print("Winner: ", winner)
-    
+
         print("-------------------------------------------------")
         print("\nPrediction based on venue:")
-        resultVenue=getVenuePrediction(team1,team2,venue)
-        if resultVenue:
-            winner=resultVenue
+        if venue:
+            resultVenue = getVenuePrediction(team1, team2, venue)
+            if resultVenue:
+                winner = resultVenue
         print("Winner: ", winner)
 
     return winner
 
+
 predictMatchResultByUserInput()
-
-
