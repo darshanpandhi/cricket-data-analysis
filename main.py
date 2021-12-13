2018551f6796ad066cebe56b8da895b9c0d41fd3
boparai1999 <boparai3@myumanitoba.ca>
2021-12-12 20:53:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from headToHead import getHeadToHeadWinner
 from teamRankings import getTeamRankingWinner
+from venue import getHomeAdvantage
 from toss import tossEvalTeams
 from datasets import generateList
 
@@ -14,19 +15,62 @@ def predictMatchResult():
         getTeamRankingWinner(team1, team2, True)
     else:
         resultHtoH = getHeadToHeadWinner(team1, team2, True)
+        winner=resultHtoH
 
     print("-------------------------------------------------")
 
     print("\nPrediction with toss result known: ")
     teamWonToss = input("Enter the team name that won the toss: ")
-
     resultToss = tossEvalTeams(team1, team2, listofMatchIds)
     if teamWonToss == team1 and resultToss[0] > resultToss[1]:
-        print("Winner: ", team1)
+        winner=team1
     elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
-        print("Winner: ", team2)
+        winner=team2
+    print("Winner: ", winner)
+
+    
+    print("-------------------------------------------------")
+    print("\nPrediction based on venue:")
+    venue = input("Enter the venue as a team name: ")
+    resultVenue=getHomeAdvantage(team1,team2)
+    if venue == team1 and resultVenue[0] > resultVenue[1]:
+        winner=team1
+    elif venue == team2 and resultVenue[1] > resultVenue[0]:
+        winner=team2
+    print("Winner: ", winner)
+
+def predictMatchResult(team1,team2,teamWonToss,venue):
+    
+    listofMatchIds = generateList(team1, team2)
+
+    if len(listofMatchIds) == 0:
+        getTeamRankingWinner(team1, team2, True)
     else:
-        print("Winner: ", resultHtoH)
+        resultHtoH = getHeadToHeadWinner(team1, team2, True)
+        winner=resultHtoH
+    
+    print("-------------------------------------------------")
+
+    print("\nPrediction with toss result known: ")
+    resultToss = tossEvalTeams(team1, team2, listofMatchIds)
+    if teamWonToss == team1 and resultToss[0] > resultToss[1]:
+        winner=team1
+    elif teamWonToss == team2 and resultToss[1] > resultToss[0]:
+        winner=team2
+    print("Winner: ", winner)
+    
+    print("-------------------------------------------------")
+    print("\nPrediction based on venue:")
+    resultVenue=getHomeAdvantage(team1,team2)
+    if venue == team1 and resultVenue[0] > resultVenue[1]:
+        winner=team1
+    elif venue == team2 and resultVenue[1] > resultVenue[0]:
+        winner=team2
+    print("Winner: ", winner)
+
+    return winner
 
 
-predictMatchResult()
+#predictMatchResult()
+(predictMatchResult("India","Pakistan","India","Pakistan"))
+
